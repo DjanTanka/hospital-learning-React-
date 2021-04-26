c9712b64c539dbaf623e393b17825be7c46003d7
TatianaVolyntseva <tvolyntseva.exceedteam@gmail.com>
2021-04-26 18:01:45 +0300

    a litle bit of functional

--- src/components/Registr.js
+++ src/components/Registr.js
@@ -1,50 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../components/Registr.scss'
-import  { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import {
   AppBar,
   Toolbar,
   Typography,
+  Container,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+  Button,
+  Snackbar,  
 } from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
 import logo from '../img/logo.png';
-import Container from '@material-ui/core/Container';
 import bigLogo from '../img/bigLogo.png';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputAdornment from '@material-ui/core/InputAdornment'
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import Button from '@material-ui/core/Button';
+import MuiAlert from '@material-ui/lab/Alert';
+
 
 const Registr = () => {
-  let history = useHistory();
-  const [values, setValues] = React.useState({
-    amount: '',
+   let history = useHistory();
+  const [values, setValues] = useState({
+    login: '',
     password: '',
-    weight: '',
-    weightRange: '',
+    repeatPassword: '',
+    rightRepeatPassword: 'false',
     showPassword: false,
   });
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
+
+  function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(true);
   };
 
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+  }
+
+
+  const handleChange = (prop) => (e) => {
+    setValues({ ...values, [prop]: e.target.value });
+    if (values.password === '' && values.repeatPassword !=="") {
+    (values.password !== values.repeatPassword)
+      ? console.log ('не совпадает')
+      : console.log ('совпадает')}
+  };
+
+  const ChangeRepeatPassword = (prop) => (e) => {
+    setValues({ ...values, [prop]: e.target.value });
+    // if (values.password !== values.repeatPassword) {
+    //   setValues(values.rightRepeatPassword = !values.rightRepeatPassword)
+    //   console.log(values.rightRepeatPassword)
+    // }
+    
+
+  }
   const handleClickShowPassword = () => {
     setValues({ ...values, showPassword: !values.showPassword });
   };
 
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
+  const handleMouseDownPassword = (e) => {
+    e.preventDefault();
   };
 
   const goToAuthor = () => {
     history.push('/author');
   }
+
+  const funcRegistration = () => {
+    (values.password !== values.repeatPassword)
+      ? console.log ('не отправляю на сервер')
+      : console.log('отправляю на сервер', values)
+    
+  }
   return (
     <>
-      <AppBar position="static" className='app'>
-        <Toolbar className="myToolBar">
-          <IconButton edge="start" className='menuButton' aria-label="menu">
+      <AppBar position='static' className='app'>
+        <Toolbar className='myToolBar'>
+          <IconButton edge='start' className='menuButton' aria-label='menu'>
             <img src={logo} alt='mainLo'/>
           </IconButton>
           <Typography className='title'>
@@ -56,32 +95,33 @@ const Registr = () => {
         <img src={bigLogo} width='375px' height='375px' alt='bigLogo' />
         <div className='registrationDiv'>
           <h1 className='containerh1'> Регистрация</h1>
-          <div className="labelInput">
+          <div className='labelInput'>
              <label>Login:</label>
              <OutlinedInput
-              className="input"
-              placeholder="Login"
-              id="login"
-              type={values.showPassword ? 'text' : 'password'}
-              value={values.password}
-              onChange={handleChange('password')}
+                className='input'
+                placeholder='Login'
+                id='login'
+                type='text'
+                value={values.login}
+                onChange={handleChange('login')}
               />
+             <Alert severity="error">This is an error message!</Alert>
           </div>
-          <div className="labelInput">
+          <div className='labelInput'>
           <label>Password:</label>
           <OutlinedInput 
-            className="input"
-            placeholder="Password"
-            id="password"
+            className='input'
+            placeholder='Password'
+            id='password'
             type={values.showPassword ? 'text' : 'password'}
             value={values.password}
             onChange={handleChange('password')}
             endAdornment={
-              <InputAdornment position="end">
+              <InputAdornment position='end'>
                 <IconButton
                   onClick={handleClickShowPassword}
                   onMouseDown={handleMouseDownPassword}
-                  edge="end"
+                  edge='end'
                 >
                   {values.showPassword ? <Visibility /> : <VisibilityOff />}
                 </IconButton>
@@ -89,30 +129,32 @@ const Registr = () => {
             }
           />
           </div>
-          <div className="labelInput">
+          <div className='labelInput'>
           <label>Repeat password:</label>
           <OutlinedInput
-            className="input"
-            placeholder="Repeat password"
-            id="repeatPassword"
+            className='input'
+            placeholder='Repeat password'
+            id='repeatPassword'
             type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
-            onChange={handleChange('password')}
+            value={values.repeatPassword}
+            onChange={ChangeRepeatPassword('repeatPassword', 'repeatRepeatPassword')}
             endAdornment={
-              <InputAdornment position="end">
+              <InputAdornment position='end'>
                 <IconButton
                   onClick={handleClickShowPassword}
                   onMouseDown={handleMouseDownPassword}
-                  edge="end"
+                  edge='end'
                 >
                   {values.showPassword ? <Visibility /> : <VisibilityOff />}
                 </IconButton>
               </InputAdornment>
+              
             }
           />
+           <Alert severity="error">Пароли должны совпадать!</Alert>
           </div>
-          <div className="registrationButtons">
-            <Button variant="outlined">Зарегистрироваться</Button>
+          <div className='registrationButtons'>
+            <Button variant='outlined' onClick ={()=>funcRegistration()}>Зарегистрироваться</Button>
             <Button onClick ={()=>goToAuthor()}>Авторизироваться</Button>
           </div>
         </div>
